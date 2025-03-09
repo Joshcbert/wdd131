4919a04e20f38f475498320609a8f4673efdf30a
joshcbert <joshcbert@gmail.com>
2025-03-08 23:58:00 -0600

    Everything

---
+++ unit_5_wdd131/recipes/main.js
@@ -0,0 +1,26 @@
+import recipes from "./recipes.mjs";
+
+// const postContainer = document.querySelector('.posts');
+
+// recipes.forEach(recipe => {
+// 	const newRecipe = document.createElement('article');
+// 	// newRecipe.id = article.id
+
+// 	newRecipe.innerHTML = `
+// 		<div class="recipe_card">
+//             <h2 class="title">${recipe.title}</h2>
+//             <img src=${recipe.imgSrc} alt=${recipe.imgAlt}>
+//             <p class="description">${recipe.description} <a href="#">Read more...</a></p>
+//         </div>
+//         <div class="details">
+//             <p class="date">${recipe.
+//                 date}</p>
+//             <p class="ages">${recipe.ages}</p>
+//             <p class="genre">${recipe.genre}</p>
+//             <p class="stars">${recipe.stars}</p>
+//                 </div>
+// 	`;
+
+// 	postContainer.appendChild(newRecipe);
+
+// })
